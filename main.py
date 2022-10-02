3b598d3b5d0415f6be98dfda99a7d69844d74b19
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-10-02 05:53:22 -0400

    refactor: adding fastapi

--- main.py
+++ main.py
@@ -1,36 +1,56 @@
-"""
-Driver code
-"""
-# pylint: disable=unused-import
-import json
-from covalent_api_lib import get_transactions
-from etherscan_lib import get_labels
-import pandas as pd
+# """
+# Driver code
+# """
+# # pylint: disable=unused-import
+# import json
+# from covalent_api_lib import get_transactions
+# from etherscan_lib import get_labels
+# import pandas as pd
 
-import scipy
-import seaborn as sns
-import numpy as np
-import matplotlib.pyplot as plt
+# import scipy
+# import seaborn as sns
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
-def main():
-    """Driver code"""
-    address = "0x0E57edbA0FccB1E388926193c873120cab961fEe"
-    transaction = get_transactions(address)
-    with open("transaction.json", "w", encoding="utf-8") as f:
-        json.dump(transaction.json(), f)
 
-    labels = get_labels(address)
-    print(labels)
-    # with open("transaction.json", "r", encoding="utf-8") as f:
-    #     transactions = json.load(f)
-    # transaction_array = transactions["data"]["items"]
-    # transaction_df = pd.DataFrame(transaction_array)
-    # transaction_df.to_csv("transaction.csv")
-    # transaction_df.drop(["from_address_label", "to_address_label"], axis=1)
-    # print(transaction_df.head())
 
+# def main():
+#     """Driver code"""
+#     address = "0x0E57edbA0FccB1E388926193c873120cab961fEe"
+#     transaction = get_transactions(address)
+#     with open("transaction.json", "w", encoding="utf-8") as f:
+#         json.dump(transaction.json(), f)
+
+#     labels = get_labels(address)
+#     print(labels)
+#     # with open("transaction.json", "r", encoding="utf-8") as f:
+#     #     transactions = json.load(f)
+#     # transaction_array = transactions["data"]["items"]
+#     # transaction_df = pd.DataFrame(transaction_array)
+#     # transaction_df.to_csv("transaction.csv")
+#     # transaction_df.drop(["from_address_label", "to_address_label"], axis=1)
+#     # print(transaction_df.head())
+
+
+# if __name__ == "__main__":
+#     main()
+
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q=None):
+    return {"item_id": item_id, "q": q}
 
 if __name__ == "__main__":
-    # ref: https://etherscan.io/apis
-    main()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
