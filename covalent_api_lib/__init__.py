175712fb87caed490599355fcf96b3e32984ec09
Earl Tankard, Jr <theflashtank21@gmail.com>
2022-10-02 03:16:43 -0400

    chore: formatting fix

--- covalent_api_lib/__init__.py
+++ covalent_api_lib/__init__.py
@@ -2,9 +2,9 @@ import os
 import requests
 from dotenv import load_dotenv
 
+
 load_dotenv()
 api_key = os.getenv("COVALENT_KEY")
-# address = "0x71C7656EC7ab88b098defB751B7401B5f6d8976F"
 BASE_URL = "https://api.covalenthq.com"
 
 
@@ -13,9 +13,6 @@ def get_transactions(address) -> requests.Response:
     transactions_endpoint = (
         BASE_URL + f"/v1/1/address/{address}/transactions_v2/?key={api_key}"
     )
-    params = {
-        "quote-currency": "USD",
-        "no-logs": True
-    }
+    params = {"quote-currency": "USD", "no-logs": True}
     transactions = requests.get(transactions_endpoint, params=params, timeout=15)
     return transactions
